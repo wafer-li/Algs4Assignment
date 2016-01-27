51b25fc2b60d14ad9bf5f2d3191e166db5fc3425
WaferLi <omyshokami@gmail.com>
2016-01-27 15:28:20 +0900

    Initial commit

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,146 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/**
+ * This is the Percolation class
+ *
+ * It use the two dimensions of boolean array
+ * and to use WeightUnionFind, transform the tow
+ * dimensions array to the one dimension one.
+ *
+ * @author Wafer
+ * @version 1.0.0
+ * @since 2016/1/27 0:36
+ */
+public class Percolation {
+
+    private boolean[][] grid;
+
+    private WeightedQuickUnionUF uf;
+
+    private int N;  // The N-by-N
+
+    public Percolation(int N) {
+
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.N = N;
+        grid = new boolean[N][N];
+        uf = new WeightedQuickUnionUF(N * N + 2);
+    }
+
+    /**
+     * Open and link the specified site;
+     * When the specified site was opened,
+     * it will search the side site of the
+     * current site.
+     * If there is opened site, it will link it.
+     *
+     * @param i The row of the specified site
+     * @param j The column of the specified site
+     */
+    public void open(int i, int j) {
+
+        if (i < 1 || j < 1 || i > N || j > N) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (!isOpen(i, j)) {
+            grid[i - 1][j - 1] = true;
+
+            /**
+             * The relationship of id and i, j is
+             * id + 1 = (i - 1) * N + j
+             * id = (i - 1) * N + j - 1
+             *
+             * The virtual top is id[N * N]
+             * The virtual bottom is id[N * N + 1]
+             */
+
+            // Link the top and the bottom to the virtual top and bottom
+            if (j == 0) {
+                if (!uf.connected((i - 1) * N + j - 1, N * N))
+                    uf.union((i - 1) * N + j - 1, N * N);
+            } else if (j == N - 1) {
+                if (!uf.connected((i - 1) * N + j - 1, N * N + 1))
+                    uf.union((i - 1) * N + j - 1, N * N + 1);
+            } else {
+                // Union the sides of it
+                if (i + 1 < N) {
+                    if (isOpen(i + 1, j)) {
+                        if (!uf.connected((i - 1) * N + j - 1, (i) * N + j - 1))
+                            uf.union((i - 1) * N + j - 1, (i) * N + j - 1);
+                    }
+                }
+                if (i - 1 > 0) {
+                    if (isOpen(i - 1, j)) {
+                        if (!uf.connected((i - 1) * N + j - 1, (i - 2) * N + j - 1))
+                            uf.union((i - 1) * N + j - 1, ((i - 2) * N) + j - 1);
+                    }
+                }
+                if (j + 1 < N) {
+                    if (isOpen(i, j + 1)) {
+                        if (!uf.connected((i - 1) * N + j - 1, (i - 1) * N + j))
+                            uf.union((i - 1) * N + j - 1, (i - 1) * N + j);
+                    }
+                }
+                if (j - 1 > 0) {
+                    if (isOpen(i, j - 1)) {
+                        if (!uf.connected((i - 1) * N + j - 1, (i - 1) * N + j - 2))
+                            uf.union((i - 1) * N + j, (i - 1) * N + j - 2);
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean isOpen(int i, int j) {
+        if (i < 1 || j < 1 || i > N || j > N) {
+            throw new IndexOutOfBoundsException();
+        }
+        return grid[i - 1][j - 1];
+
+    }
+
+    public boolean isFull(int i, int j) {
+
+        if (i < 1 || j < 1 || i > N || j > N) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        // if the site is connected to the top
+        return uf.connected((i - 1) * N + j - 1, N * N);
+    }
+
+    public boolean percolates() {
+        // if the top is connected to the bottom
+        return uf.connected(N * N, N * N + 1);
+    }
+
+
+    public static void main(String[] args) {
+        // Test demo
+
+        Percolation percolation = new Percolation(10);
+
+        while (!percolation.percolates()) {
+            int i = StdRandom.uniform(1, 11);
+            int j = StdRandom.uniform(1, 11);
+
+            percolation.open(i, j);
+
+            StdOut.println("Open " + i + " , " + j + " Site");
+
+            if (percolation.isFull(i, j)) {
+                StdOut.println("The " + i + " , " + j + " is FULL");
+            } else {
+                StdOut.println("The " + i + " , " + j + " is not FULL");
+            }
+        }
+        StdOut.println("The graph is percolated!!");
+    }
+
+}
---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,42 @@
+/**
+ * This is the percolationStats class.
+ * Using the Union-Find algorithm to simulate the percolation.
+ * And to find the probabilities of the percolation
+ *
+ * @author Wafer
+ * @version 1.0.0
+ * @since 2016/1/27 0:30
+ */
+public class PercolationStats {
+
+    private int examTimes;
+
+    public PercolationStats(int N, int T) {
+        Percolation percolation = new Percolation(N);
+
+    }
+
+    public double mean() {
+
+        return 0;
+    }
+
+    public double stddev() {
+
+        return 0;
+    }
+
+    public double confidenceLo() {
+
+        return 0;
+    }
+
+    public double confidenceHi() {
+
+        return 0;
+    }
+
+    public static void main(String [] args) {
+        Percolation percolation = new Percolation(10);
+    }
+}
