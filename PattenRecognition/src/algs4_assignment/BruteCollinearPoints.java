576fa9139137dbb739c2f3f8ca3bd4406d991bdd
Wafer-Li <omyshokami@gmail.com>
2016-03-09 00:33:38 +0900

    Initialize PattenRecognition

---
+++ PattenRecognition/src/algs4_assignment/BruteCollinearPoints.java
@@ -0,0 +1,12 @@
+package algs4_assignment;
+
+/**
+ * This is the BruteCollinearPoints class.
+ * Please put more info.
+ *
+ * @author Wafer
+ * @version 1.0.0
+ * @since 16/3/9 00:28
+ */
+public class BruteCollinearPoints {
+}
