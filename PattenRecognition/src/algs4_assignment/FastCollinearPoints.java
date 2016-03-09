25303b0c7bf861c8839017f10bf10e22140216df
Wafer-Li <omyshokami@gmail.com>
2016-03-09 20:17:23 +0900

    Add sample client to each solutions

--- PattenRecognition/src/algs4_assignment/FastCollinearPoints.java
+++ PattenRecognition/src/algs4_assignment/FastCollinearPoints.java
@@ -1,5 +1,9 @@
 package algs4_assignment;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -78,5 +82,34 @@ public class FastCollinearPoints {
 
     public LineSegment[] segments() {
         return collinear;
+    }
+
+    public static void main(String[] args) {
+
+        // read the N points from a file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.show(0);
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
     }
 }
