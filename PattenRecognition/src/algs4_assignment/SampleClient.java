a0be73496418e71cf60a48471c71f11f9ccde90e
Wafer-Li <omyshokami@gmail.com>
2016-03-09 13:04:09 +0900

    Add sample client

---
+++ PattenRecognition/src/algs4_assignment/SampleClient.java
@@ -0,0 +1,44 @@
+package algs4_assignment;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * This is the SampleClient class.
+ * Please put more info.
+ *
+ * @author Wafer
+ * @version 1.0.0
+ * @since 16/3/9 13:02
+ */
+public class SampleClient {
+    public static void main(String[] args) {
+
+        // read the N points from a file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.show(0);
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+    }
+}
