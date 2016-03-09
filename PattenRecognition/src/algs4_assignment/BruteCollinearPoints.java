2c24d6a54799a2e91aa04ca625ba667fc635b483
Wafer-Li <omyshokami@gmail.com>
2016-03-09 15:49:41 +0900

    Optimize the brute force solution according to the hints

--- PattenRecognition/src/algs4_assignment/BruteCollinearPoints.java
+++ PattenRecognition/src/algs4_assignment/BruteCollinearPoints.java
@@ -12,28 +12,50 @@ import java.util.ArrayList;
  */
 public class BruteCollinearPoints {
 
-    private Point[] points;
     private LineSegment[] collinear;
 
     public BruteCollinearPoints(Point[] points) {
-        this.points = points;
+
+        if (points == null) {
+            throw new NullPointerException();
+        }
+
         ArrayList<LineSegment> lineSegments = new ArrayList<>();
 
+        // Check four points P, Q, R, S
         for (int i = 0; i < points.length; i++) {
-            for (int j = i + 1; j < points.length; j++) {
-                for (int k = j + 1; k < points.length; k++) {
-                    for (int w = k + 1; w < points.length; w++) {
-                        // Check the three slopes of four points
-                        // Points: p, q, r, s
+            checkNullPointer(points[i]);
 
-                        double slopePToQ = points[i].slopeTo(points[j]);
-                        double slopePToR = points[i].slopeTo(points[k]);
+            for (int j = i + 1; j < points.length; j++) {
+                checkNullPointer(points[j]);
+                checkRepeatedPoint(points[i], points[j]);
+
+                double slopePToQ = points[i].slopeTo(points[j]);
+
+                for (int k = j + 1; k < points.length; k++) {
+                    checkNullPointer(points[k]);
+
+                    checkRepeatedPoint(points[i], points[k]);
+                    checkRepeatedPoint(points[j], points[k]);
+
+                    double slopePToR = points[i].slopeTo(points[k]);
+
+                    // P Q R not at the same line, skip.
+                    if (slopePToQ != slopePToR) {
+                        continue;
+                    }
+
+                    for (int w = k + 1; w < points.length; w++) {
+                        checkNullPointer(points[w]);
+
+                        checkRepeatedPoint(points[i], points[w]);
+                        checkRepeatedPoint(points[j], points[w]);
+                        checkRepeatedPoint(points[k], points[w]);
+
                         double slopePToS = points[i].slopeTo(points[w]);
 
-                        if (slopePToQ == slopePToR) {
-                            if (slopePToR == slopePToS) {
-                                lineSegments.add(new LineSegment(points[i], points[w]));
-                            }
+                        if (slopePToR == slopePToS) {
+                            lineSegments.add(new LineSegment(points[i], points[w]));
                         }
                     }
                 }
@@ -42,6 +64,19 @@ public class BruteCollinearPoints {
 
         collinear = new LineSegment[lineSegments.size()];
         collinear = lineSegments.toArray(collinear);
+    }
+
+
+    private void checkNullPointer(Point point) {
+        if (point == null) {
+            throw new NullPointerException();
+        }
+    }
+
+    private void checkRepeatedPoint(Point p1, Point p2) {
+        if (p1.compareTo(p2) == 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int numberOfSegments() {
