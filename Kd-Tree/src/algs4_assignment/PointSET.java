756cc3c2fe2b0f4f432e162261f45a508a1de67d
Wafer-Li <omyshokami@gmail.com>
2016-03-20 00:24:26 +0900

    Initialize kd-tree

---
+++ Kd-Tree/src/algs4_assignment/PointSET.java
@@ -0,0 +1,50 @@
+package algs4_assignment;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+/**
+ * This is the PointSET class.
+ * The brute force implementation of
+ *
+ * @author Wafer
+ * @version 1.0.0
+ * @since 16/3/9 00:27
+ */
+public class PointSET {
+
+    public PointSET() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void insert() {
+    }
+
+    public boolean contains() {
+        return false;
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+
+    public static void main(String [] args) {
+
+    }
+}
