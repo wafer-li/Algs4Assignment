6236d95f4819e11dddd91c653dc4acdcbfe99b07
wafer-li <omyshokami@gmail.com>
2016-03-20 21:59:31 +0900

    Finish the brute force algorithm

--- Kd-Tree/src/algs4_assignment/PointSET.java
+++ Kd-Tree/src/algs4_assignment/PointSET.java
@@ -2,6 +2,10 @@ package algs4_assignment;
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This is the PointSET class.
@@ -13,35 +17,102 @@ import edu.princeton.cs.algs4.RectHV;
  */
 public class PointSET {
 
+    private SET<Point2D> point2DSET;
+
+    /**
+     * Construct an empty set of points
+     * I use the SET class of algs4.jar
+     */
     public PointSET() {
-
+        point2DSET = new SET<>();
     }
 
+    /**
+     * Is the set empty?
+     *
+     * @return True if the set is empty.
+     */
     public boolean isEmpty() {
-        return false;
+        return point2DSET.isEmpty();
     }
 
+    /**
+     * Number of the points of the set
+     * @return The number of the set points
+     */
     public int size() {
-        return 0;
+        return point2DSET.size();
     }
 
-    public void insert() {
+    /**
+     * Insert the points into the set,
+     * if it is NOT inside the set
+     *
+     * @param p The point to be inserted
+     */
+    public void insert(Point2D p) {
+        if (!point2DSET.contains(p)) {
+            point2DSET.add(p);
+        }
     }
 
-    public boolean contains() {
-        return false;
+    /**
+     * If the set contain the point?
+     *
+     * @param p The specific point
+     * @return True if contains.
+     */
+    public boolean contains(Point2D p) {
+        return point2DSET.contains(p);
     }
 
+    /**
+     * Draw all the points to std draw
+     */
     public void draw() {
-
+        for (Point2D point
+                : point2DSET) {
+            point.draw();
+        }
     }
 
+    /**
+     * All the points inside the rectangle
+     * @param rect The specific rectangle
+     * @return The points inside the rectangle
+     */
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        List<Point2D> point2DList = new ArrayList<>();
+
+        for (Point2D point
+                : point2DSET) {
+            if (rect.contains(point)) {
+                point2DList.add(point);
+            }
+        }
+
+        return point2DList;
     }
 
     public Point2D nearest(Point2D p) {
-        return null;
+        double distance = -1;
+        Point2D goal = null;
+
+        for (Point2D point
+                : point2DSET) {
+            if (distance == -1) {
+                distance = p.distanceTo(point);
+                goal = point;
+            } else {
+                double tempDistance = p.distanceTo(point);
+                if (tempDistance < distance) {
+                    distance = tempDistance;
+                    goal = point;
+                }
+            }
+        }
+
+        return goal;
     }
 
     public static void main(String [] args) {
