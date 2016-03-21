af34a0e5d8ebb10ce424772cd536a6b3caab9280
wafer-li <omyshokami@gmail.com>
2016-03-22 03:11:17 +0900

    Add KdTree generator

---
+++ Kd-Tree/src/algs4_assignment/KdTreeGenerator.java
@@ -0,0 +1,29 @@
+package algs4_assignment; /******************************************************************************
+ * Compilation:  javac KdTreeGenerator.java
+ * Execution:    java KdTreeGenerator N
+ * Dependencies:
+ * <p>
+ * Creates N random points in the unit square and print to standard output.
+ * <p>
+ * % java KdTreeGenerator 5
+ * 0.195080 0.938777
+ * 0.351415 0.017802
+ * 0.556719 0.841373
+ * 0.183384 0.636701
+ * 0.649952 0.237188
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class KdTreeGenerator {
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        for (int i = 0; i < N; i++) {
+            double x = StdRandom.uniform(0.0, 1.0);
+            double y = StdRandom.uniform(0.0, 1.0);
+            StdOut.printf("%8.6f %8.6f\n", x, y);
+        }
+    }
+}
